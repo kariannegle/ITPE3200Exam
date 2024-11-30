83c0aa5cbc1b31d7895d7fdb319c384e8dd7d1de
Amiin <amiin20004@gmail.com>
2024-11-30 15:05:25 +0100

    Renamed files from js to jsx and deleted unnessary files that was genereated with react

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,7 +5,6 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-
 const container = document.getElementById("root")
 const root = createRoot(container)
 
